72a86d5a85263ff709dcad8c3f81f4985f3f59dd
andy-h-2016 <andy@moesif.com>
2023-07-27 17:32:54 -0700

    Move dashboards side by side

--- my-dev-portal/src/components/moesif/moesif-embedded-template.js
+++ my-dev-portal/src/components/moesif/moesif-embedded-template.js
@@ -1,25 +1,27 @@
 export default function MoesifEmbeddedTemplate(props) {
   return (
-    <div className="page-layout__focus">
-      <div className="iframeWrapper">
-        <iframe
-          title="Moesif Live Event Log"
-          id="preview-frame"
-          src={props.iFrameSrcLiveEvent}
-          name="preview-frame"
-          noresize="noresize"
-        ></iframe>
+    <div className="dashboards page-layout__focus">
+      <div className="dashboards-wrapper">
+        <div className="iframeWrapper">
+          <iframe
+            title="Moesif Live Event Log"
+            id="preview-frame"
+            src={props.iFrameSrcLiveEvent}
+            name="preview-frame"
+            noresize="noresize"
+          ></iframe>
+        </div>
+        <div className="iframeWrapper">
+          <iframe
+            title="Moesif Time Series"
+            id="preview-frame"
+            src={props.iFrameSrcTimeSeries}
+            name="preview-frame"
+            noresize="noresize"
+          ></iframe>
+        </div>
+        {props.error}
       </div>
-      <div className="iframeWrapper">
-        <iframe
-          title="Moesif Time Series"
-          id="preview-frame"
-          src={props.iFrameSrcTimeSeries}
-          name="preview-frame"
-          noresize="noresize"
-        ></iframe>
-      </div>
-      {props.error}
     </div>
   );
 }
