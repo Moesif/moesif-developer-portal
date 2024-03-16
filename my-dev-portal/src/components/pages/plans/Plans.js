d8737386d8347cd93b8c429e1b021455410565cf
xinghengwang <xing@moesif.com>
2024-03-15 19:57:59 -0700

    add note about stripe price table

--- my-dev-portal/src/components/pages/plans/Plans.js
+++ my-dev-portal/src/components/pages/plans/Plans.js
@@ -4,22 +4,9 @@ import { PageLayout } from "../../page-layout";
 import MoesifPlans from "./MoesifPlans";
 
 function Plans(props) {
-  const useStripePriceTable = process.env.REACT_APP_USE_STRIPE_PRICE_TABLE;
-
   return (
     <PageLayout>
-      {!useStripePriceTable ? (
-        <MoesifPlans />
-      ) : (
-        <div className="page-layout__focus">
-          <div className="stripe-pricing-table-container">
-            <stripe-pricing-table
-              pricing-table-id={process.env.REACT_APP_STRIPE_PRICING_TABLE_ID}
-              publishable-key={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
-            ></stripe-pricing-table>
-          </div>
-        </div>
-      )}
+      <MoesifPlans />
     </PageLayout>
   );
 }
--- my-dev-portal/src/components/stripe/StripeProducts.js
+++ my-dev-portal/src/components/stripe/StripeProducts.js
@@ -2,6 +2,9 @@ import React from "react";
 import Auth0StripeProducts from "./Auth0StripeProducts";
 import OktaStripeProducts from "./OktaStripeProducts";
 
+// This is deprecated
+// This is you prefer to use Stripe Price table, which offers less customization
+
 export default function StripeProducts() {
   if (process.env.REACT_APP_AUTH_PROVIDER === "Okta") {
     return <OktaStripeProducts />;
