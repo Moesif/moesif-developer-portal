3f3f92c74039e938646e5eebe8298886b1195ed7
xinghengwang <xing@moesif.com>
2024-11-23 12:43:45 -0800

    support scss

--- my-dev-portal/src/index.js
+++ my-dev-portal/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "https://js.stripe.com/v3/pricing-table.js";
-import "./styles/styles.css";
+import "./styles/styles.scss";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
