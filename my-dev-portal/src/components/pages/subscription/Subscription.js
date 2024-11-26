83b0e22bec7261cf488770fdea214abbbd055fe4
xinghengwang <xing@moesif.com>
2024-11-26 15:00:52 -0800

    update wording

--- my-dev-portal/src/components/pages/subscription/Subscription.js
+++ my-dev-portal/src/components/pages/subscription/Subscription.js
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 import usePlans from "../../../hooks/usePlans";
 import NoticeBox from "../../notice-box";
 import noPriceIcon from "../../../images/icons/empty-state-price.svg";
-import SubDisplay from './SubDisplay';
+import SubDisplay from "./SubDisplay";
 
 function Subscription(props) {
   const { isAuthenticated, isLoading, user } = useAuthCombined();
@@ -30,8 +30,8 @@ function Subscription(props) {
             description={
               <span>
                 If you just purchased a plan, please{" "}
-                <strong>wait at least 10 to 15 minutes</strong>{" "}
-                for the systems to sync.
+                <strong>wait at least 10 to 15 minutes</strong> for the systems
+                to sync.
               </span>
             }
             actions={
@@ -55,11 +55,6 @@ function Subscription(props) {
               </>
             }
           />
-          <p className="text-muted">
-            For developers, if you want subscriptions to sync faster, you can
-            locally cache the subscription in your system. In this example
-            project, there is no local database or storage.
-          </p>
         </>
       )}
 
@@ -70,6 +65,11 @@ function Subscription(props) {
           ))}
         </div>
       )}
+      <p className="text-muted">
+        Newly created subscriptions may take up to 10 to 15 minutes to sync. For developers, if you want subscriptions to sync faster, you can
+        locally cache the subscription in your system. In this example project,
+        there is no local database or storage.
+      </p>
     </PageLayout>
   );
 }
