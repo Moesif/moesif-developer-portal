5c2d3981009cdd338803509c4cd724b82a32ed79
xinghengwang <xing@moesif.com>
2024-11-25 20:44:17 -0800

    support background image and new hero image

--- my-dev-portal/src/components/page-layout.js
+++ my-dev-portal/src/components/page-layout.js
@@ -2,9 +2,9 @@ import React from "react";
 import { NavBar } from "./navigation/desktop/nav-bar";
 import { MobileNavBar } from "./navigation/mobile/mobile-nav-bar";
 
-export const PageLayout = ({ hideNavTabs, children }) => {
+export const PageLayout = ({ hideNavTabs, children, isHome }) => {
   return (
-    <div className="page-layout">
+    <div className={`page-layout ${isHome ? 'page-layout--home' : ''}`}>
       <NavBar hideNavTabs={hideNavTabs} />
       <MobileNavBar hideNavTabs={hideNavTabs} />
       <div className="page-layout__content">{children}</div>
--- my-dev-portal/src/components/pages/home/Home.js
+++ my-dev-portal/src/components/pages/home/Home.js
@@ -5,11 +5,11 @@ import MoesifPlans from "../plans/MoesifPlans";
 import { SignupButton } from "../../buttons/signup-button";
 import { LoginButton } from "../../buttons/login-button";
 
-import heroImage from "../../../images/assets/hero-image.png";
+import heroImage from "../../../images/assets/dev-portal-hero.svg";
 
 function Home() {
   return (
-    <PageLayout>
+    <PageLayout isHome>
       <section className="hero">
         <div className="hero-content">
           <h1>My Dev Portal!</h1>
