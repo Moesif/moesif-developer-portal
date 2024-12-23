312b883552c0677087ba9665f94648aae66fdde6
xinghengwang <xing@moesif.com>
2024-12-23 10:23:23 -0800

    Update vite.config.js

--- vite-front-end/vite.config.js
+++ vite-front-end/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 4000, // Replace 3006 with your desired port number
+  },
 })
