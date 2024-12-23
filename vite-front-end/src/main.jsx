a97c8df4d50ac46a00b9c592297cc6a04986a4f7
xinghengwang <xing@moesif.com>
2024-12-23 09:50:03 -0800

    added moesif

--- vite-front-end/src/main.jsx
+++ vite-front-end/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import moesifBrowser from "moesif-browser-js";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  // </StrictMode>,
 )
+
+if (import.meta.env.VITE_MOESIF_PUBLISHABLE_APPLICATION_ID) {
+  moesifBrowser.init({
+    applicationId: import.meta.env.VITE_MOESIF_PUBLISHABLE_APPLICATION_ID,
+    // add other option here
+  });
+  if (window) {
+    window.moesif = moesifBrowser;
+  }
+} else {
+  console.log(
+    "Please add using REACT_APP_MOESIF_PUBLISHABLE_APPLICATION_ID to .env to enable Moesif Browser JS to track actions such as sign up"
+  );
+}
